fde524a31df3157bd9838d424f0637f1dcde11b6
Andrew Perry <pezza76@hotmail.com>
2025-05-02 11:41:36 +0000

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
-		return (0);
 
 	size_t left_height = binary_tree_height(tree->left);
 	size_t right_height = binary_tree_height(tree->right);
+
+	
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
